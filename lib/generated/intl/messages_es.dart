4c69ed5f1a0ef37b2a8b37c7525984ecb089a856
Carlos David Dzul Canul <kalindzulcanul@gmail.com>
2024-10-22 22:25:26 -0600

    added generated intl file

---
+++ lib/generated/intl/messages_es.dart
@@ -0,0 +1,87 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'es';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "classifierButton":
+            MessageLookupByLibrary.simpleMessage("Lista de Clasificadores"),
+        "classifierInfoButton":
+            MessageLookupByLibrary.simpleMessage("Que son los clasificadores?"),
+        "classifierTitle":
+            MessageLookupByLibrary.simpleMessage("Clasificadores"),
+        "classifier_list0":
+            MessageLookupByLibrary.simpleMessage("Para mitades o pedazos"),
+        "classifier_list1":
+            MessageLookupByLibrary.simpleMessage("Para haces y manojos"),
+        "classifier_list10": MessageLookupByLibrary.simpleMessage(
+            "Para contar azotes o campanadas"),
+        "classifier_list11":
+            MessageLookupByLibrary.simpleMessage("Para contar brazadas"),
+        "classifier_list12": MessageLookupByLibrary.simpleMessage(
+            "Para contar veces en que se realiza una acción"),
+        "classifier_list13": MessageLookupByLibrary.simpleMessage(
+            "Para contar cosas sobrepuestas, estibado"),
+        "classifier_list14": MessageLookupByLibrary.simpleMessage(
+            "Para contar cosas largas y delgadas"),
+        "classifier_list15": MessageLookupByLibrary.simpleMessage(
+            "Para contar personas y animales, vivos o muertos"),
+        "classifier_list16": MessageLookupByLibrary.simpleMessage(
+            "Para hojas de libro o algo delgado y plano"),
+        "classifier_list17":
+            MessageLookupByLibrary.simpleMessage("Para contar algo redondo"),
+        "classifier_list18": MessageLookupByLibrary.simpleMessage(
+            "Para contar un pedazo, fragmento o retazo arrancado"),
+        "classifier_list19": MessageLookupByLibrary.simpleMessage(
+            "Para contar un pedazo, fragmento o retazo cortado"),
+        "classifier_list2": MessageLookupByLibrary.simpleMessage(
+            "Para pedazo que haya sido quebrado de algo rígido"),
+        "classifier_list3":
+            MessageLookupByLibrary.simpleMessage("Para contar las plantas"),
+        "classifier_list4":
+            MessageLookupByLibrary.simpleMessage("Para contar atados"),
+        "classifier_list5": MessageLookupByLibrary.simpleMessage(
+            "Para algo que se transporta en la espalda"),
+        "classifier_list6": MessageLookupByLibrary.simpleMessage(
+            "Para contar veces que se a sucedido o se ha hecho algo"),
+        "classifier_list7": MessageLookupByLibrary.simpleMessage(
+            "Para la cantidad de objetos que una persona alcanza cargar entre los brazos"),
+        "classifier_list8":
+            MessageLookupByLibrary.simpleMessage("Para montón, amontonamiento"),
+        "classifier_list9": MessageLookupByLibrary.simpleMessage(
+            "Para cosas o objetos inanimados"),
+        "configurationOption0": MessageLookupByLibrary.simpleMessage("Idioma"),
+        "configurationOption1":
+            MessageLookupByLibrary.simpleMessage("Método de texto"),
+        "configurationTitle":
+            MessageLookupByLibrary.simpleMessage("Configuración"),
+        "languageOption0": MessageLookupByLibrary.simpleMessage("Español"),
+        "languageOption1": MessageLookupByLibrary.simpleMessage("Ingles"),
+        "numberFieldHint":
+            MessageLookupByLibrary.simpleMessage("ingresa un numero"),
+        "title": MessageLookupByLibrary.simpleMessage(
+            "Convertidor Avanzado de Numerales Mayas al Cuadrado"),
+        "titleSectionOne":
+            MessageLookupByLibrary.simpleMessage("De numero a glifos"),
+        "titleSectionTwo":
+            MessageLookupByLibrary.simpleMessage("De glifos a numero")
+      };
+}
